19e6ddd40796009d6b87b210026b83fd0e675f36
Alessya Tkachenko <tkachenko.alessya@gmail.com>
2025-11-10 20:23:48 +0500

    feat: add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,11 +13,55 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const result = [];
+  const deleted = [];
+  //const skip = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    //if (skip.has(i)) continue;
+    if (typeof arr[i] == "string") {
+      switch (arr[i]) {
+        case "--discard-next":
+          if (typeof arr[i + 1] !== "string") {
+            i++;
+            deleted.push(i);
+          }
+          break;
+        case "--discard-prev":
+          if (i != 0 && typeof arr[i - 1] !== "string") {
+            if (deleted.includes(i - 1)) {
+              break;
+            }
+            result.splice(result.length - 1, 1);
+            deleted.push(i);
+          }
+          break;
+        case "--double-next":
+          if (i + 1 < arr.length && typeof arr[i + 1] !== "string") {
+            result.push(arr[i + 1]);
+          }
+          break;
+        case "--double-prev":
+          if (deleted.includes(i - 1)) {
+            break;
+          }
+          if (i != 0 && typeof arr[i - 1] !== "string") {
+            if (i - 1 >= 0) {
+              result.push(arr[i - 1]);
+            }
+          }
+          break;
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
