2b6ac9067b51be5d3adbf8f27b5456058394f7cf
Alessya Tkachenko <tkachenko.alessya@gmail.com>
2025-11-10 22:01:12 +0500

    feat: add encodeLine function

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given a string, return its encoding version.
@@ -11,11 +11,26 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  const strArr = str.split("");
+  result = [];
+  let count = 1;
+
+  strArr.forEach((char, index) => {
+    if (char === strArr[index + 1] && index < strArr.length) {
+      count++;
+    } else {
+      if (count > 1) {
+        result.push(count.toString() + char.toString());
+        count = 1;
+      } else {
+        result.push(char);
+      }
+    }
+  });
+  return result.join("");
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
